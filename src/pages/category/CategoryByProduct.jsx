e46b8e1c7c623bf048f9749983d2e430d0b8509c
Wind <136448822+WindLapTrinh@users.noreply.github.com>
2024-10-18 09:22:40 +0700

    update layout

--- src/pages/category/CategoryByProduct.jsx
+++ src/pages/category/CategoryByProduct.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Box, Tabs } from "zmp-ui";
 import ProductList from "@/pages/home/ProductList";
+import CustomHeader from "../shared/pages/CustomHeader";
+import CustomBottomNavigation from "../shared/components/CustomBottomNavigation";
 import "../../css/detailhome/product/categoryProduct.css";
 
 const CategoryByProduct = ({ categories, gotoCategory }) => {
@@ -11,29 +13,37 @@ const CategoryByProduct = ({ categories, gotoCategory }) => {
   };
 
   return (
-    <Box className="by-product">
-      <Box className="category-by">
-        <Box className="slider-by bg-white p-4">
-          <div className="tabs-wrapper">
-            <Tabs className="horizontal-tabs" id="contact-list" scrollable="true">
-              <Tabs.Tab key="tab1" label="Vegetables">
-                <ProductList />
-              </Tabs.Tab>
-              <Tabs.Tab key="tab2" label="Điện thoại">
-                <ProductList />
-              </Tabs.Tab>
-              <Tabs.Tab key="tab3" label="Máy tính">
-                <ProductList />
-              </Tabs.Tab>
-              <Tabs.Tab key="tab4" label="Đồng hồ">
-                <ProductList />
-              </Tabs.Tab>
-              <Tabs.Tab key="tab5" label="Tablet">
-                <ProductList />
-              </Tabs.Tab>
-            </Tabs>
-          </div>
+    <Box>
+      <Box className="by-product">
+        <CustomHeader title={"Danh mục sản phẩm"} showBackIcon={true}/>
+        <Box className="category-by">
+          <Box className="slider-by bg-white p-4">
+            <div className="tabs-wrapper">
+              <Tabs
+                className="horizontal-tabs"
+                id="contact-list"
+                scrollable="true"
+              >
+                <Tabs.Tab key="tab1" label="Vegetables">
+                  <ProductList />
+                </Tabs.Tab>
+                <Tabs.Tab key="tab2" label="Điện thoại">
+                  <ProductList />
+                </Tabs.Tab>
+                <Tabs.Tab key="tab3" label="Máy tính">
+                  <ProductList />
+                </Tabs.Tab>
+                <Tabs.Tab key="tab4" label="Đồng hồ">
+                  <ProductList />
+                </Tabs.Tab>
+                <Tabs.Tab key="tab5" label="Tablet">
+                  <ProductList />
+                </Tabs.Tab>
+              </Tabs>
+            </div>
+          </Box>
         </Box>
+        <CustomBottomNavigation/>
       </Box>
     </Box>
   );
